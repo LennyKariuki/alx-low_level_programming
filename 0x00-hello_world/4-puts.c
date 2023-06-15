d39398e0ab7b16df59f9bc85c2fc5eedb7857aa0
LennyKariuki <kariukileonard20@gmail.com>
2023-06-15 05:10:26 -0700

    C programming is easy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
- *
+ * main - prints Programming is easy
+ Return a new line puzzle
  * Return: 0 (Success)
  */
-int main(void)
-{
-	printf("Programming is like building a multilingual puzzle\n");
 
-	return 0;
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return(0);
 }
