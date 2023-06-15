31fce098662011d0526d2bcd2c775c6cf4e6c8b8
LennyKariuki <kariukileonard20@gmail.com>
2023-06-15 12:02:17 -0700

    My project

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
-
 /**
- * main - print a C program that prints the size of various types.
- * You should produce the exact same output
- * Return: Always 0.
+ * A code that prints size of different user types
+ * Return 0 (Success)
  */
-
 int main(void)
-
 {
-	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of a int: %zu byte(s)\n", sizeof(int));
-        printf("Size of a long int: %zu byte(s)\n",sizeof(long int));
-	printf("Size of a long long: %zu byte(s)\n",sizeof(long long int));
-	printf("Size of a float %zu byte(s)\n", sizeof(float));
-	return (0);
+	char l;
+	int e;
+	long int n;
+	long long int y;
+	float f;
+printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof (l));
+printf("Size of an Int: %lu byte(s)\n", (unsigned long)sizeof (e));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof (n));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(y));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof (f));
+return (0);
 }
