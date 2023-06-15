e21f61260a78a02224c5ffac41d6d9b3aad32be7
LennyKariuki <kariukileonard20@gmail.com>
2023-06-15 12:22:37 -0700

    Hey kindly output the correct code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 /**
- * A code that prints size of different user types
+ *mainm- A code that prints size of different user types
  * Return 0 (Success)
  */
 int main(void)
 {
-	char l;
-	int e;
-	long int n;
-	long long int y;
+	char a;
+	int b;
+	long int c;
+	long long int d;
 	float f;
-printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof (l));
-printf("Size of an Int: %lu byte(s)\n", (unsigned long)sizeof (e));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof (n));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(y));
+printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof (a));
+printf("Size of an Int: %lu byte(s)\n", (unsigned long)sizeof (b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof (c));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d)); 
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof (f));
 return (0);
 }
