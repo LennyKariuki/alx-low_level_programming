f735d3235ad880b5202a9b771294a2551b8f850a
LennyKariuki <kariukileonard20@gmail.com>
2023-06-15 14:25:05 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,9 @@
 /**
  * main - prints "and that piece of art is useful" -  Dora Korpar, 2015-10-19",
  * followed by a new line, to the standard error.
- * Return: Always 0 (Success)
+ * Return: Always 1
  */
+
 
 int main(void)
 
