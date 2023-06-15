bd4e859aa1c80e73ed447f540d3f6f61156dc045
LennyKariuki <kariukileonard20@gmail.com>
2023-06-15 14:57:30 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 
 /**
  * main - print "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- * followed by a new line
+ * followed by a new line.
  * Return: Always 1.
  */
 
 int main(void)
 
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
