15b79c5a03ee591a13e4019c022113ea9d0db28f
LennyKariuki <kariukileonard20@gmail.com>
2023-06-15 06:02:57 -0700

    I hope youll work mahn

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
+ * main - prints "with proper grammar, but the outcome is 
+ * a piece of art" followed newline
+ * Return: Always 0.
  *
- * Description: Prints the sentence "with proper grammar, but the outcome is a piece of art,"
- *              followed by a new line.
- *
- * Return: Always 0 (Success)
  */
+
 int main(void)
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return 0;
+	return (0);
 }
