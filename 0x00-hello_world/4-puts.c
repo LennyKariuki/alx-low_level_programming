4bfb94a905e64fe1ce7f0867798e24b42b83636e
LennyKariuki <kariukileonard20@gmail.com>
2023-06-15 05:19:01 -0700

    10th error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
+
 /**
  * main - prints Programming is easy
  Return a new line puzzle
- * Return: 0 (Success)
+ * Return: Always 0.
  */
+
 
 int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 }
