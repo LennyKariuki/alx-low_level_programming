16984d57253305a6b467999ea3ab36e69a42c668
LennyKariuki <kariukileonard20@gmail.com>
2023-06-20 04:08:22 -0700

    ./1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabts
+ * Compute and display the output
+ */
+
+void print_alphabet(void)
+{
+	char k;
+
+	for (k = 'a'; k  <= 'z'; k++)
+		_putchar(k);
+
+	_putchar('\n');
+}
