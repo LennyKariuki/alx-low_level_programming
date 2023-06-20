0c66841bb9aca3990d41fd015d6c3e2a960f3803
LennyKariuki <kariukileonard20@gmail.com>
2023-06-20 12:08:09 -0700

    Hello today is Tuesday

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer..
- * Display..
+ * jack_bauer - prints every minute of the day of Jack Bauer.
+ * Display
  * starting from 00:00 to 23:59
  */
 void jack_bauer(void)
@@ -24,7 +24,7 @@ void jack_bauer(void)
 			_putchar('\n');
 			l++;
 		}
-		l++;
+		k++;
 	}
 }
 
