c6a1e677528811a5a9d538a886b5a4f8812152c1
LennyKariuki <kariukileonard20@gmail.com>
2023-06-26 03:11:15 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @x: string
+ * Display
+ * Return: length
+ */
+
+int _strlen(char *x)
+{
+	int longi = 0;
+
+	while (*x != '\0')
+	{
+		longi++;
+		x++;
+	}
+
+	return (longi);
+}
