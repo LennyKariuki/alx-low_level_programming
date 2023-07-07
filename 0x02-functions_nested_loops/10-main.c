0482ecb9544809f33f358c6cf18b5d73212a602e
LennyKariuki <kariukileonard20@gmail.com>
2023-07-07 13:12:08 -0700

    Hello world i am coding

--- 0x02-functions_nested_loops/10-main.c
+++ 0x02-functions_nested_loops/10-main.c
@@ -1,16 +1,20 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * add - adds two integers and returns the result
- * @a: int to be added to b
- * @b: int to be added to a
- * Return: sum of a and b
- */
+int add(int a, int b);
+
+#endif /* MAIN_H */
 int add(int a, int b)
 {
-	int c;
-
-	c = a + b;
-
-	return (c);
+    /* Implementation of the add function */
+    int c;
+    c = a + b;
+    return c;
+}
+int add(int a, int b)
+{
+    /* Implementation of the add function */
+    int c;
+    c = a + b;
+    return c;
 }
