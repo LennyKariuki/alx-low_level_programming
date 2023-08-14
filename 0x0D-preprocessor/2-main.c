b093b7961582c532395b5a0e2d8024dc4417b148
LennyKariuki <kariukileonard20@gmail.com>
2023-08-14 11:38:29 -0700

    Updated code

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- *  * main - prints the name of the file.
- *   *
- *    * Return: Always 0.
- *     */
+ *  *main - pritns the name of the program
+ *   *Return: 0 on sucess
+ *    */
 int main(void)
 {
 		printf("%s\n", __FILE__);
